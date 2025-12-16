9ceeb7b56e19f47ad0066e11e2167fb994541874
Serhii Basok <sergeybsk19@gmail.com>
2025-12-16 15:22:59 +0200

    Add: implement init_db for initialize tables

---
+++ app/config/init_db.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from app.config.settings import settings
+from app.models.base import Base
+from app.models.cars import CarModel
+
+
+async def init_db():
+    engine = create_async_engine(settings.ASYNC_DATABASE_URL, echo=True)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    await engine.dispose()
+    print("All tables created!")
+
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
