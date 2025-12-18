ac34c2c41dae789741ffe75e0d49adab34ab7166
Serhii Basok <sergeybsk19@gmail.com>
2025-12-18 07:27:31 +0200

    Add: extract_phone

--- app/parser/extract_data.py
+++ app/parser/extract_data.py
@@ -1,10 +1,10 @@
-import re
-from bs4 import Tag
 import random
+import re
+from playwright.async_api import Page, async_playwright
+from bs4 import Tag
 from bs4 import BeautifulSoup
 import asyncio
-from playwright.async_api import async_playwright
-import logging
+
 
 VIN_RE = re.compile(r"[A-HJ-NPR-Z0-9]{17}")
 CAR_NUMBER_RE = re.compile(r"\b[A-ZА-ЯІЇЄ]{2}\s?\d{4}\s?[A-ZА-ЯІЇЄ]{2}\b")
@@ -61,3 +61,21 @@ def extract_odometer(car_card: Tag) -> int:
         return int("".join(filter(str.isdigit, odometer_text))) * 1000
     except Exception:
         return 0
+
+
+async def extract_number(url: str) -> str:
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page = await browser.new_page()
+
+        await page.goto(url)
+        await page.click('#sellerInfo button[data-action="showBottomPopUp"]')
+
+        phone_link = await page.wait_for_selector('a.action-wrapper-link[href^="tel:"]')
+
+        phone = (await phone_link.get_attribute("href")).replace("tel:", "")
+        phone_number = re.sub(r"[^\d+]", "", phone)
+        if not phone_number.startswith("38"):
+            phone_number = "38" + phone_number[-10:]
+        await browser.close()
+        return int(phone_number)
